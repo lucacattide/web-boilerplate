231199f8e18089800b4b2185daf5c2911644fd94
Luca Cattide <l.cattide@email.it>
2018-04-04 09:50:46 +0200

    Upgrade:
    - Implementazione meta tags SEO & OpenGraph;

--- php/head.php
+++ php/head.php
@@ -1,7 +1,20 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta property="og:url" content="" />
+<meta property="og:type" content="" />
+<meta property="og:title" content="" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="" />
+<meta property="og:audio" content="" />
+<meta property="og:video" content="" />
+<meta property="og:locale" content="it_IT" />
+<meta property="og:locale:alternate" content="" />
+<meta property="og:type" content="" />
+<meta property="og:site_name" content="" />
 <!--Fine Meta Tags-->
 <!--Inizio Preload-->
 <link rel="preload" href="../css/dist/main.css" as="style">
