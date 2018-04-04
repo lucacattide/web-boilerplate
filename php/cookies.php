4bf500387cdf8a51fb3d9342038d3e0e288047e8
Luca Cattide <l.cattide@email.it>
2018-04-04 09:39:05 +0200

    Upgrade:
    - Implementazione boilerplate banner cookies e pulsante torna su;

---
+++ php/cookies.php
@@ -0,0 +1,14 @@
+<!--Inizio Cookies-->
+<section class="cookies nascondi">
+  <h6>Cookie Policy</h6>
+  <p class="cookies__corpo">utilizza la tecnologia dei Cookies - tecnici e di terze parti - per offrire la
+    migliore esperienza Web possibile. La navigazione su prevede il consenso spontaneo
+    ad accettare i termini e delle condizioni previste.<br />Per ulteriori dettagli
+    <a class="cookies__corpo__link" href="privacy" title="Cookie Policy - Informativa"
+      tabindex="100">consultate la pagina dedicata</a>.</p>
+  <aside class="cookies__chiudi disabilita">
+    <h6>Chiudi</h6>
+    X
+  </aside>
+</section>
+<!--Fine Cookies-->
---
+++ php/torna-su.php
@@ -0,0 +1,8 @@
+<!--Inizio Torna Su-->
+<aside class="torna-su occulta">
+  <h6>Torna Su</h6>
+  <div class="torna-su__corpo">
+    &lt;
+  </div>
+</aside>
+<!--Fine Torna Su-->
