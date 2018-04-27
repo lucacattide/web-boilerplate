6fc3978a79b24ef34f0f40e6553035b660e237eb
Luca Cattide <l.cattide@email.it>
2018-04-27 10:10:17 +0200

    Upgrade: - Implementazione Codice di Condotta;
    Update:
    - Dichiarazione locale documento;
    - Leggimi;

--- php/doctype.php
+++ php/doctype.php
@@ -4,8 +4,8 @@
   <html lang="" prefix="og: http://ogp.me/ns#">
   <!--<![endif]-->
 <!--[if IE 8]>
-  <html class="no-js lt-ie9" lang="en" prefix="og: http://ogp.me/ns#">
+  <html class="no-js lt-ie9" lang="" prefix="og: http://ogp.me/ns#">
 <![endif]-->
 <!--[if gt IE 8]>
-  <html class="no-js" prefix="og: http://ogp.me/ns#">
+  <html class="no-js" lang="" prefix="og: http://ogp.me/ns#">
 <![endif]-->
