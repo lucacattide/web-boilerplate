5718a50d6f24de8eb2789c57b439d2139c3e16f5
Luca Cattide <l.cattide@email.it>
2018-06-19 10:33:25 +0200

    Update:
    - Dipendenze Node;
    - Refactoring browser hacks;
    - Refactoring cookies;

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ import '../sass/index.scss';
 // Importazione Librerie
 import $ from 'jquery';
 import Modernizr from 'modernizr';
+import gestisciCookies from './cookies.js';
 import inizializzaTornaSu from './torna-su.js';
 /**
  * if (!Modernizr.pointerevents && !Modernizr.touchevents) {
@@ -14,5 +15,6 @@ import inizializzaTornaSu from './torna-su.js';
 
 // Main
 $(document).ready(() => {
+  gestisciCookies();
   inizializzaTornaSu();
 })
