6396d12c7fbf863e83f49738bf0e7c86f61606ed
Luca Cattide <luca.cattide@greylab.it>
2018-11-19 09:47:01 +0100

    Update:
    - Dipendenze Node;
    - Configurazione Gulp;

--- gulpfile.js
+++ gulpfile.js
@@ -4,39 +4,35 @@
 // Inclusione Moduli
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
-const {phpMinify} = require('@cedx/gulp-php-minify');
 const size = require('gulp-size');
 
 // Tasks
 // HTML
 gulp.task('html', () => gulp.src('./html/dist/**/*.html')
-  .pipe(htmlmin({
-    removeComments: true,
-    collapseWhitespace: true,
-  }))
-  .pipe(size())
-  .pipe(gulp.dest('./html/dist'))
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+    }))
+    .pipe(size())
+    .pipe(gulp.dest('./html/dist'))
 );
 // PHP
-gulp.task('php', () => gulp.src('./php/dist/**/*.php', {
-    read: false,
-  })
-  .pipe(phpMinify())
-  .pipe(htmlmin({
-    removeComments: true,
-    collapseWhitespace: true,
-  }))
-  .pipe(size())
-  .pipe(gulp.dest('./php/dist'))
+gulp.task('php', () => gulp.src('./php/dist/**/*.php')
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+    }))
+    .pipe(size())
+    .pipe(gulp.dest('./php/dist'))
 );
 // Index
 gulp.task('index', () => gulp.src(['./*.html', './*.php'])
-  .pipe(htmlmin({
-    removeComments: true,
-    collapseWhitespace: true,
-  }))
-  .pipe(size())
-  .pipe(gulp.dest('./dist'))
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+    }))
+    .pipe(size())
+    .pipe(gulp.dest('./dist'))
 );
 // Produzione
 gulp.task('dist', [
